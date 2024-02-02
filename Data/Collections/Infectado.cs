b0a49a43670de091f207d8c76348ee6c1afd4665
KayoLeanndro <kayoleanndro2@gmail.com>
2024-02-02 13:31:51 -0300

    initiating repository & connecting to mongo

---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apimongodio.Data.Collections
+{
+    public class Infectado
+    {
+        
+    }
+}
