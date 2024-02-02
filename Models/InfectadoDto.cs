ed8b04828a4edc80c6b9a6176df88b1556a59a07
KayoLeanndro <kayoleanndro2@gmail.com>
2024-02-02 18:46:23 -0300

    puting the classes

---
+++ Models/InfectadoDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apimongodio.Models
+{
+    public class InfectadoDto
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
